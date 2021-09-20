cbb815c16862ede6938cdf09c4ea03d4238701d3
Ivan Grohotov <vanyagrohotov@mail.ru>
2021-09-20 12:39:12 +0700

    Added files and changes required for deployment to heroku

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -269,3 +270,9 @@ LOGGING = {
         },
     },
 }
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
